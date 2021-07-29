e41286626f86728cf405e412189456b9d1a5f3fa
TriiiHaus <nguytri8598@gmail.com>
2021-07-28 21:57:34 -0700

    Added Map onto html, changed header colors, rounded buttons

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// TO MAKE THE MAP APPEAR YOU MUST
+// ADD YOUR ACCESS TOKEN FROM
+// https://account.mapbox.com
+// mapboxgl.accessToken = 'pk.eyJ1IjoidHJpaWloYXVzIiwiYSI6ImNrcm9ja2s5aTZmM3AydnBkaXVwank3cHAifQ.7lG7dllcNDPKoe99U3hBDg';
+
+// var onpageMap = document.getElementById('map')
+// var map = new mapboxgl.Map({
+//   container: 'map', // container id
+//   style: 'mapbox://styles/mapbox/streets-v11', // style URL
+//   center: [-74.5, 40], // starting position [lng, lat]
+//   zoom: 9 // starting zoom
+// // });
